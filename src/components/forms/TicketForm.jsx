28a9d0c3699a98a2ea0a403233d7e5b574290a92
Chelsea Brooks <cbrooks6212@gmail.com>
2024-05-29 14:30:21 -0400

    finished project

--- src/components/forms/TicketForm.jsx
+++ src/components/forms/TicketForm.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
 import "./Form.css";
+import { createTicket } from "../../services/ticketService.jsx";
+import { useNavigate } from "react-router-dom";
 
-export const TicketForm = () => {
+export const TicketForm = ({currentUser}) => {
+  const [ticket, setTicket] = useState({ description: "", emergency: false});
+
+  const navigate =useNavigate()
+
+  const handleSave = (event) => {
+    event.preventDefault()
+    if(ticket.description) {
+const newTicket = {
+    userId: currentUser.id,
+    description: ticket.description,
+    emergency: ticket.emergency,
+    dateCompleted: "",
+}
+createTicket(newTicket).then(() => {
+navigate("/tickets")
+})
+    }else{
+        window.alert("Please fill out description!")
+    }
+  }
+
   return (
     <form>
       <h2>New Service Ticket</h2>
@@ -11,6 +35,11 @@ export const TicketForm = () => {
             type="text"
             className="form-control"
             placeholder="Brief description of problem"
+            onChange={(event) => {
+              const ticketCopy = { ...ticket };
+              ticketCopy.description = event.target.value;
+              setTicket(ticketCopy);
+            }}
           />
         </div>
       </fieldset>
@@ -18,13 +47,20 @@ export const TicketForm = () => {
         <div className="form-group">
           <label>
             Emergency:
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              onChange={(event) => {
+                const ticketCopy = { ...ticket };
+                ticketCopy.emergency = event.target.checked;
+                setTicket(ticketCopy)
+              }}
+            />
           </label>
         </div>
       </fieldset>
       <fieldset>
         <div className="form-group">
-            <button className="form-btn btn-info">Submit Ticket</button>
+          <button className="form-btn btn-info" onClick={handleSave}>Submit Ticket</button>
         </div>
       </fieldset>
     </form>
